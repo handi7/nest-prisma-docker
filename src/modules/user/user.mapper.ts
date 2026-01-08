acb97ea1e1b4512592f517de9d25048bafdd3f30
handi <handi7.co@gmail.com>
2026-01-08 14:58:22 +0700

    update user mapper

--- src/modules/user/user.mapper.ts
+++ src/modules/user/user.mapper.ts
@@ -1,19 +1,16 @@
-import { User } from 'src/types/User';
-import {
-  toRoleWithPermissionNames,
-  toRoleWithPermissions,
-} from '../role/role.mapper';
+import { User } from "src/types/User";
+import { toRoleWithPermissionNames, toRoleWithPermissions } from "../role/role.mapper";
 
 export function toUserWithRoleAndPermissions(user: User) {
   return {
     ...user,
-    role: toRoleWithPermissions(user.role as any),
+    role: toRoleWithPermissions(user.role),
   };
 }
 
 export function toUserWithRoleAndPermissionNames(user: User) {
   return {
     ...user,
-    role: toRoleWithPermissionNames(user.role as any),
+    role: toRoleWithPermissionNames(user.role),
   };
 }
